5493e5c9210926a54d42a8e979a8cf83a90bf57c
Никта Агеев <nvageev_1@edu.hse.ru>
2023-09-26 18:50:00 +0500

    Добавил тесты и переписал main.py

--- src/algorithms/fast_fibonacci/function.py
+++ src/algorithms/fast_fibonacci/function.py
@@ -2,13 +2,28 @@ import sys
 
 sys.set_int_max_str_digits(1000000000)
 
-def mulmatrix2x2(a: tuple[int, int, int, int], b: tuple[int, int, int, int]) -> tuple[int, int, int, int]:
+
+def mulmatrix2x1(a: (int, int), b: (int, int, int, int)) -> (int, int):
+    """
+    returns result of the multiplication of 2x2 and 2x1
+
+    Parameters:
+        a (tuple[int, int, int, int]): a first matrix
+        b (tuple[int, int, int, int]): a second matrix
+
+    Returns:
+        multiplication (tuple[int, int]): a * b
+    """
+    return a[0] * b[0] + a[1] * b[2], a[0] * b[1] + a[1] * b[3]
+
+
+def mulmatrix2x2(a: (int, int, int, int), b: (int, int, int, int)) -> (int, int, int, int):
     """
     returns the product of multiplication of two 2x2 matrices: a * b
 
-        Parameters:
-            a (tuple[int, int, int, int]): a first matrix
-            b (tuple[int, int, int, int]): a second matrix
+    Parameters:
+         a (tuple[int, int, int, int]): a first matrix
+         b (tuple[int, int, int, int]): a second matrix
 
         Returns:
             multiplication (tuple[int, int, int, int]): a * b
@@ -16,33 +31,33 @@ def mulmatrix2x2(a: tuple[int, int, int, int], b: tuple[int, int, int, int]) -> 
     return a[0] * b[0] + a[1] * b[2], a[0] * b[1] + a[1] * b[3], a[2] * b[0] + a[3] * b[2], a[2] * b[1] + a[3] * b[3]
 
 
-def mulmatrix2x1(a: tuple[int, int], b: tuple[int, int, int, int]) -> tuple[int, int]:
-    return a[0] * b[0] + a[1] * b[2], a[0] * b[1] + a[1] * b[3]
+def fibonacci(n: int) -> (int, int):
+    """
+    count n- fibonacci number
 
+    Parameters:
+        n (int) - order number
 
-def fibonacci(n: int) -> int:
+    Returns:
+        fibonacci number
+    """
     a = (0, 1, 1, 1)
     b = (1, 0, 0, 1)
+
     while n > 0:
         if n % 2 == 1:
             b = mulmatrix2x2(b, a)
         n //= 2
         a = mulmatrix2x2(a, a)
+
     return mulmatrix2x1((0, 1), b)
 
 
-def check(n: int) -> None:
-    if n < 1:
-        raise ValueError("n должно быть больше 0")
-
-
 def main(n: str):
-    try:
-        N = int(n)
-    except ValueError:
-        raise ValueError("n должно быть целым числом")
-    check(N)
-    return {'result': str(fibonacci(N)[0])}
+    if not str(n).isnumeric():
+        raise ValueError("n должно быть натуральным числом")
+
+    return {'result': str(fibonacci(int(n))[0])}
 
 
 if __name__ == '__main__':
--- src/algorithms/fast_fibonacci/tests.py
+++ src/algorithms/fast_fibonacci/tests.py
@@ -1,7 +1,7 @@
 import unittest
 
 
-from src.algorithms.fast_fibonacci.function import main
+from src.algorithms.fast_fibonacci.function import *
 
 
 class TestCase(unittest.TestCase):
@@ -11,8 +11,11 @@ class TestCase(unittest.TestCase):
         for index, number in enumerate(self.numbers):
             self.assertEqual(main(str(index + 1)), {'result': str(number)})
 
-    def test_fibonacci_2(self):
-        pass
+    def test_matrix_mul_1(self):
+        self.assertEqual((5, 8), mulmatrix2x1((2, 1), (1, 2, 3, 4)))
+
+    def test_matrix_mul_2(self):
+        self.assertEqual((5, 8, 7, 10), mulmatrix2x2((2, 1, 1, 2), (1, 2, 3, 4)))
 
 
 if __name__ == '__main__':
