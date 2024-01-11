31ff5caa9025b6a9bfb37c66fca2660b5c1ce341
MikhailovAlexander <avmikhaylov_3@edu.hse.ru>
2024-01-11 22:49:02 +0500

    Добавил задание по НОД

---
+++ src/algorithms/gcd_slow/function.py
@@ -0,0 +1,47 @@
+import time
+
+
+ERR_A_NOT_INT_MSG = 'Значение параметра a не является целым числом'
+ERR_B_NOT_INT_MSG = 'Значение параметра b не является целым числом'
+ERR_BOTH_ZERO_MSG = 'Значения параметров a и b равны нулю'
+
+
+def gcd(a: int, b: int) -> int:
+    """Вычисляет наибольший общий делитель двух целых чисел.
+    Медленная итеративная реализация.
+
+    :param a: Целое число a.
+    :param b: Целое число b.
+    :raise TypeError: Если a или b не являются целыми числами.
+    :raise ValueError: Если a и b равны нулю.
+    :return: Значение наибольшего общего делителя.
+    """
+    pass
+
+
+def main(a: int, b: int) -> dict[str: int, str: float]:
+    """Вычисляет наибольший общий делитель двух целых чисел и
+    продолжительность вычисления.
+
+    :param a: Целое число a.
+    :param b: Целое число b.
+    :raise TypeError: Если a или b не являются целыми числами.
+    :raise ValueError: Если a и b равны нулю.
+    :return: Словарь со значениями НОД и длительности вычисления.
+    """
+    start_time = time.time()
+    gcd_val = gcd(a, b)
+    duration = time.time() - start_time
+    return {'gcd': gcd_val, 'duration': duration}
+
+
+if __name__ == '__main__':
+    a = -12
+    b = -3
+    print('Для небольших чисел НОД вычисляется быстро:')
+    print(f'a = {a}, b = {b}, gcd = {gcd(a,b)}')
+
+    a = 1000000000
+    b = 2
+    print('Вычисление НОД с указанными параметрами займет некоторое время...')
+    print(main(a, b))
